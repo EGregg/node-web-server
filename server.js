37dee35bd01de6d597639d9f3bd7036a80ea68dc
Creosino <creosino@gmail.com>
2016-12-09 22:31:41 -0500

    added middleware and maintenance page

--- server.js
+++ server.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  //need to update to es6 injections when I figure out why node 6.2 isnt using them
+  var log = ''+ now + ':'+req.method + ''+req.url;
+
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
+// app.use((req,res, next)=>{
+//   res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () =>{
   return new Date().getFullYear()
+});
+
+hbs.registerHelper('getCurrentYear', ()=>{
+  return new Date().getFullYear();
 });
 
 hbs.registerHelper('screamIt', (text) => {
